aa34c9f187b621e9f0e31dc2f5a179d456a228d3
Berk Can Geyikçi <31792851+br33z3@users.noreply.github.com>
2022-01-17 00:37:07 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,5 @@
 import os
 
-
-os.system("git clone https://github.com/br33z3/Nature_JScraper/tree/main/LinkFinder")
-os.system("cp LinkFinder/README.md .")
 os.system("python3 LinkFinder/setup.py install")
 os.system("pip3 install -r  LinkFinder/requirements.txt")
 os.system("pip3 install pyfiglet argparse html==1.7")
